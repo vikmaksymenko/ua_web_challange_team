8bc9d942b8ae1797bd07ff26c0a0d96b4336feee
Konstantin <azizov-0997@mail.ru>
2015-09-12 19:08:44 +0000

    Resize query and fix search for user dublicate

--- php/register.php
+++ php/register.php
@@ -17,14 +17,13 @@ $postUsername = htmlspecialchars($_POST['username']);
 $postEmail = htmlspecialchars($_POST['email']);
 $postPassword = htmlspecialchars($_POST['password']);
 
-//values to be inserted in database table
-$email = '"'.$mysqli->real_escape_string($postEmail).'"';
-$password = '"'.$mysqli->real_escape_string($postPassword).'"';
 
-$query = "SELECT email FROM users WHERE email = ? AND password = ?";
+
+//Searching if no same user created already
+$query = "SELECT email FROM users WHERE email = ? OR username = ?";
 $statement = $mysqli->prepare($query);
 
-$statement->bind_param('ss', $postEmail, $postPassword);
+$statement->bind_param('ss', $postEmail, $postUsername);
 
 $statement->execute();
 
@@ -32,10 +31,16 @@ $statement->bind_result($email);
 $matchingUser = $statement->fetch();
 
 $statement->close();
+
 if($matchingUser[0].$email) {
     echo 'false';
 } else {
-    $insert_row = $mysqli->query("INSERT INTO users (email, password) VALUES($email, $password)");
+    //values to be inserted in database table
+    $username = '"'.$mysqli->real_escape_string($postUsername).'"';
+    $email = '"'.$mysqli->real_escape_string($postEmail).'"';
+    $password = '"'.$mysqli->real_escape_string($postPassword).'"';
+
+    $insert_row = $mysqli->query("INSERT INTO users (username, email, password) VALUES($username, $email, $password)");
 
     if($insert_row) {
         echo 'true';
