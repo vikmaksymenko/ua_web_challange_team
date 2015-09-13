bbd7d6a4fc54f069b54daafa256e22e9b0c97638
Konstantin <azizov-0997@mail.ru>
2015-09-13 14:12:32 +0000

    Fix submit_result API

--- php/surveys/submit_result.php
+++ php/surveys/submit_result.php
@@ -5,7 +5,9 @@ $mysqli = connectToMySQL();
 
 $query = "INSERT INTO results (email, data, survey_id) VALUES (?, ?, ?)";
 $stmt = $mysqli->prepare($query);
-$stmt->bind_param('ssi', $_POST['email'], $_POST['data'], $_POST['survey_id']);
+$null = NULL;
+$stmt->bind_param('sbi', $_POST['email'], $null, $_POST['survey_id']);
+$stmt->send_long_data(1, $_POST['data']);
 if($stmt->execute()) {
     echo 'true';
 } else {
